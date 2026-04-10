491100f010677949ce56189dc61bb9deaefa4200
Rayana Almeida <rayana.almeida@icomp.ufam.edu.br>
2026-04-10 11:48:18 -0400

    feat: add header file for graph data structure and related functions

---
+++ grafo.h
@@ -0,0 +1,12 @@
+#ifndef GRAFO_H
+#define GRAFO_H
+#include <stdbool.h>
+#include "lista_vizinhos.h"
+typedef no_t* grafo_t; 
+grafo_t grafo_criar(int tamanho);
+bool lista_vizinhos_adicionar(int vizinho, lista_vizinhos_t **lista);
+void lista_vizinhos_imprimir(lista_vizinhos_t *lista);
+void grafo_atualizar_vizinhos(int tam, double raio_comunicacao, grafo_t grafo);
+void grafo_imprimir(int tam, grafo_t grafo);
+void grafo_destruir(int tam, grafo_t grafo);
+#endif
