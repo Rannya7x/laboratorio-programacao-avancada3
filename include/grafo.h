14fa280b7e47ee036a623e3420f16d74131167dc
Rayana Almeida <rayana.almeida@icomp.ufam.edu.br>
2026-04-11 13:44:32 -0400

    feat: add typedef for no_t in grafo.h

--- include/grafo.h
+++ include/grafo.h
@@ -2,6 +2,7 @@
 #define GRAFO_H
 #include <stdbool.h>
 #include "lista_vizinhos.h"
+typedef struct no_t no_t;
 typedef no_t* grafo_t; 
 grafo_t grafo_criar(int tamanho);
 bool lista_vizinhos_adicionar(int vizinho, lista_vizinhos_t **lista);
