62767ae43d4259b9c6d93fb9a74f0827e2a828a7
Rayana Almeida <rayana.almeida@icomp.ufam.edu.br>
2026-04-11 14:14:53 -0400

    refactor: remove unused function declarations from grafo.h

--- include/grafo.h
+++ include/grafo.h
@@ -5,8 +5,6 @@
 typedef struct no_t no_t;
 typedef no_t* grafo_t; 
 grafo_t grafo_criar(int tamanho);
-bool lista_vizinhos_adicionar(int vizinho, lista_vizinhos_t **lista);
-void lista_vizinhos_imprimir(lista_vizinhos_t *lista);
 void grafo_atualizar_vizinhos(int tam, double raio_comunicacao, grafo_t grafo);
 void grafo_imprimir(int tam, grafo_t grafo);
 void grafo_destruir(int tam, grafo_t grafo);
