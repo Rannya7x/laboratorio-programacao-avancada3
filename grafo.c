58b3706d31b2f6db0bafd85271aa0c46a4047454
Rayana Almeida <rayana.almeida@icomp.ufam.edu.br>
2026-04-11 14:42:47 -0400

    feat: add function to update the graphs list and remove struct definition for no_t and clean up grafo.c

--- grafo.c
+++ grafo.c
@@ -1,12 +1,8 @@
 #include "stdlib.h"
+#include "math.h"
 #include "include/lista_vizinhos.h"
 #include "include/grafo.h"
-struct no_t {
-    int id;
-    double pos_x;
-    double pos_y;
-    lista_vizinhos_t *lista_vizinhos;
-};
+
 
 grafo_t grafo_criar(int tamanho){
     grafo_t grafo = (grafo_t) malloc(tamanho * sizeof(no_t));
@@ -14,5 +10,15 @@ grafo_t grafo_criar(int tamanho){
     return grafo;
 }
 
-
-
+void grafo_atualizar_vizinhos(int tam, double raio_comunicacao, grafo_t grafo){
+    for(int i = 0; i < tam; i++){
+        for(int j = 0; j < tam; j++){
+            if(i != j){
+                double distancia = sqrt(pow(grafo[i].pos_x - grafo[j].pos_x, 2) + pow(grafo[i].pos_y - grafo[j].pos_y, 2));
+                if(distancia < raio_comunicacao){
+                    lista_vizinhos_adicionar(grafo[i].id, &grafo[j].lista_vizinhos);
+                }
+            }
+        }
+    }
+}
